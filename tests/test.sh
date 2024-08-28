e7f5f9ae7ed6597b8ef0f49458a149b474763bf7
JeremyWesthead <jeremywesthead3@gmail.com>
2024-08-28 16:07:26 +0100

    feat: gnomonicus 3.0.0

--- tests/test.sh
+++ tests/test.sh
@@ -38,44 +38,41 @@ pip install gnomonicus==$gnomonicus_version
 
 #These are made up test cases to hit edge cases
 #Most of these use COVID-19 as it is quick to process, but some use a fake genome
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_E484K-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv --minor_populations $(pwd)/tests/test-cases/no-minors.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_E484K-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 1
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_F2F-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv --minor_populations $(pwd)/tests/test-cases/no-minors.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_F2F-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 3
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_F2L-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv --minor_populations $(pwd)/tests/test-cases/no-minors.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_F2L-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 4
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_200_indel-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv --minor_populations $(pwd)/tests/test-cases/no-minors.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_200_indel-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 5
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-double-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv --minor_populations $(pwd)/tests/test-cases/no-minors.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-double-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 6
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample "$(pwd)/tests/test-cases/NC_045512.2-S_E484K&1450_ins_a-minos.vcf" --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv --minor_populations $(pwd)/tests/test-cases/no-minors.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample "$(pwd)/tests/test-cases/NC_045512.2-S_E484K&1450_ins_a-minos.vcf" --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 7
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-minors.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv --minor_populations $(pwd)/tests/test-cases/minor_alleles.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-minors.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 8
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-minors.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue-COV.csv --minor_populations $(pwd)/tests/test-cases/minor_alleles.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-minors.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue-COV.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 9
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/TEST-DNA.gbk --sample $(pwd)/tests/test-cases/TEST-DNA-large-del.vcf --catalogue $(pwd)/tests/test-cases/TEST-DNA-catalogue.csv --minor_populations $(pwd)/tests/test-cases/minor_alleles.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/TEST-DNA.gbk --sample $(pwd)/tests/test-cases/TEST-DNA-large-del.vcf --catalogue $(pwd)/tests/test-cases/TEST-DNA-catalogue.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 10
 
 #These are realistic TB cases
-#As they're TB, pre-pickle the genome to save time
-gbkToPkl tests/test-cases/NC_000962.3.gbk --compress
-
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_000962.3.gbk.pkl --sample $(pwd)/tests/test-cases/NC_000962_3_test_0001.vcf --catalogue $(pwd)/tests/test-cases/NC_000962_3_catalogue_1.csv --minor_populations $(pwd)/tests/test-cases/minor_alleles.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_000962.3.gbk --sample $(pwd)/tests/test-cases/NC_000962_3_test_0001.vcf --catalogue $(pwd)/tests/test-cases/NC_000962_3_catalogue_1.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 11
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_000962.3.gbk.pkl --sample $(pwd)/tests/test-cases/NC_000962_3_test_0002.vcf --catalogue $(pwd)/tests/test-cases/NC_000962_3_catalogue_1.csv --minor_populations $(pwd)/tests/test-cases/minor_alleles.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_000962.3.gbk --sample $(pwd)/tests/test-cases/NC_000962_3_test_0002.vcf --catalogue $(pwd)/tests/test-cases/NC_000962_3_catalogue_1.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 12
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_000962.3.gbk.pkl --sample $(pwd)/tests/test-cases/NC_000962_3_test_0003.vcf --catalogue $(pwd)/tests/test-cases/NC_000962_3_catalogue_1.csv --minor_populations $(pwd)/tests/test-cases/minor_alleles.txt --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_000962.3.gbk --sample $(pwd)/tests/test-cases/NC_000962_3_test_0003.vcf --catalogue $(pwd)/tests/test-cases/NC_000962_3_catalogue_1.csv  --gvcf $(pwd)/tests/test-cases/empty.gvcf --null_positions $(pwd)/tests/test-cases/no-null-positions.txt --testing true
 keepOutput 13
 
 
@@ -83,10 +80,10 @@ keepOutput 13
 python3 $(pwd)/tests/build-null-gvcf.py --fasta_path $(pwd)/tests/test-cases/NC_045512.2.all_n.fasta --output_path $(pwd)/tests/test-cases/NC_045512.2.all_n.gvcf
 python3 $(pwd)/tests/build-null-gvcf.py --fasta_path $(pwd)/tests/test-cases/NC_000962.3.all_n.fasta --output_path $(pwd)/tests/test-cases/NC_000962.3.all_n.gvcf
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_E484K-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv --minor_populations $(pwd)/tests/test-cases/no-minors.txt --gvcf $(pwd)/tests/test-cases/NC_045512.2.all_n.gvcf --null_positions $(pwd)/tests/test-cases/covid-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_045512.2.gbk --sample $(pwd)/tests/test-cases/NC_045512.2-S_E484K-minos.vcf --catalogue $(pwd)/tests/test-cases/NC_045512.2-test-catalogue.csv  --gvcf $(pwd)/tests/test-cases/NC_045512.2.all_n.gvcf --null_positions $(pwd)/tests/test-cases/covid-null-positions.txt --testing true
 keepOutput 14
 
-sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_000962.3.gbk.pkl --sample $(pwd)/tests/test-cases/NC_000962_3_test_0001.vcf --catalogue $(pwd)/tests/test-cases/NC_000962_3_catalogue_1.csv --minor_populations $(pwd)/tests/test-cases/minor_alleles.txt --gvcf $(pwd)/tests/test-cases/NC_000962.3.all_n.gvcf --null_positions $(pwd)/tests/test-cases/tb-null-positions.txt --testing true
+sudo nextflow run . -profile docker --reference $(pwd)/tests/test-cases/NC_000962.3.gbk --sample $(pwd)/tests/test-cases/NC_000962_3_test_0001.vcf --catalogue $(pwd)/tests/test-cases/NC_000962_3_catalogue_1.csv  --gvcf $(pwd)/tests/test-cases/NC_000962.3.all_n.gvcf --null_positions $(pwd)/tests/test-cases/tb-null-positions.txt --testing true
 keepOutput 15
 
 
